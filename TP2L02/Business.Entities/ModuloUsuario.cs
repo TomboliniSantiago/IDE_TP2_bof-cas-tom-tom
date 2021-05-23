747e04d57452a0bfa3d65388722c854dc29478fc
TomboliniSantiago <81423722+TomboliniSantiago@users.noreply.github.com>
2021-05-23 18:29:25 -0300

    Add files via upload

---
+++ TP2L02/Business.Entities/ModuloUsuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class ModuloUsuario : BusinessEntity
+    {
+        private int idU, idM;
+        private bool pB, pA, pM, pC;
+        public int IdUsuario { get=>this.idU; set => this.idU=value; }
+        public int IdModulo { get => this.idM; set => this.idM=value; }
+        public bool Permitebaja { get => this.pB; set => this.pB=value; }
+        public bool PermiteAlta { get => this.pA; set => this.pA=value; }
+        public bool PermiteMod { get => this.pM; set => this.pM=value; }
+        public bool PermiteConsulta { get => this.pC; set => this.pC=value; }
+    }
+}
